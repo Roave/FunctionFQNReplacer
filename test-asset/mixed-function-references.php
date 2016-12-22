8aab10283e49828cbe6f60599c0c22b011a32b60
Marco Pivetta <ocramius@gmail.com>
2016-12-22 20:18:40 +0100

    Updating examples to consider case sensitivity issues too

--- test-asset/mixed-function-references.php
+++ test-asset/mixed-function-references.php
@@ -49,3 +49,39 @@ namespace Foo {
     // replaced with absolute reference to \Foo\foo_namespace_function
     foo_namespace_function();
 }
+// now the same, but with case sensitivity changes
+namespace Foo {
+    use function Baz\imported_Function;
+    use function absolute_Imported_function;
+    // not replaced
+    IMPORTED_FUNCTION();
+    // not replaced
+    ABSOLUTE_IMPORTED_FUNCTION();
+    // not replaced
+    \FOO\ABSOLUTE_FUNCTION();
+    // not replaced
+    BAR\RELATIVE_FUNCTION_TO_REPLACE();
+    // replaced with absolute reference to \absolute_function_to_replace
+    ABSOLUTE_FUNCTION_TO_REPLACE();
+    // not replaced
+    \ABSOLUTE_FUNCTION();
+    // functions existing in \
+    // replaced with absolute reference to \base_namespace_function
+    BASE_NAMESPACE_FUNCTION();
+    // not replaced
+    \BASE_NAMESPACE_FUNCTION();
+    // functions existing in \Bar
+    // not replaced
+    \BAR\BAR_NAMESPACE_FUNCTION();
+    // not replaced (wrong reference)
+    BAR\BAR_NAMESPACE_FUNCTION();
+    // replaced with absolute reference to \bar_namespace_function
+    BAR_NAMESPACE_FUNCTION();
+    // functions existing in Foo
+    // not replaced
+    \FOO\FOO_NAMESPACE_FUNCTION();
+    // not replaced (wrong reference)
+    FOO\FOO_NAMESPACE_FUNCTION();
+    // replaced with absolute reference to \Foo\foo_namespace_function
+    FOO_NAMESPACE_FUNCTION();
+}
