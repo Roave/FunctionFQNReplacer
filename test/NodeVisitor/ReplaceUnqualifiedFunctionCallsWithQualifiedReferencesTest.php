2c893dd254d5748b8ffeba50bc61b4472857a71d
Marco Pivetta <ocramius@gmail.com>
2016-12-22 20:53:41 +0100

    Aliased functions are not imported

--- test/NodeVisitor/ReplaceUnqualifiedFunctionCallsWithQualifiedReferencesTest.php
+++ test/NodeVisitor/ReplaceUnqualifiedFunctionCallsWithQualifiedReferencesTest.php
@@ -211,4 +211,32 @@ final class ReplaceUnqualifiedFunctionCallsWithQualifiedReferencesTest extends P
         $visitor->leaveNode($namespace);
         self::assertNull($visitor->afterTraverse([$namespace]));
     }
+
+    public function testDoesNotReplaceUnknownImportedAliasedFunction()
+    {
+        $namespace    = new Namespace_(new Name('bar'));
+        $use          = new Use_([new UseUse(new Name('baz\\bar'), 'foo')], Use_::TYPE_FUNCTION);
+        $functionCall = new FuncCall(new Name('foo'));
+
+        $namespace->stmts[] = $use;
+        $namespace->stmts[] = $functionCall;
+
+        $visitor = new ReplaceUnqualifiedFunctionCallsWithQualifiedReferences(function () {
+            self::fail('Not expected to be called');
+        });
+
+        self::assertNull($visitor->beforeTraverse([$namespace]));
+        self::assertNull($visitor->enterNode($namespace));
+        self::assertNull($visitor->enterNode($use));
+        self::assertNull($visitor->leaveNode($use));
+        self::assertNull($visitor->enterNode($functionCall));
+
+        $replaced = $visitor->leaveNode($functionCall);
+
+        self::assertNotSame($functionCall, $replaced);
+        self::assertEquals(new FuncCall(new Name('foo')), $replaced);
+
+        $visitor->leaveNode($namespace);
+        self::assertNull($visitor->afterTraverse([$namespace]));
+    }
 }
